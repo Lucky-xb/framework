e15a7f4d9a757ead777eef286d2f77fedbd59305
Lucky-xb <x_bin94@163.com>
2021-08-18 12:11:55 +0800

    M：修改HTTP请求

--- src/mgr/NetMgr.ts
+++ src/mgr/NetMgr.ts
@@ -13,11 +13,47 @@ export class NetMgr extends BaseIns implements INetMgr {
         return super.ins as NetMgr;
     }
 
-    private byte: Laya.Byte;
+    private _byte: Laya.Byte;
 
     public init() {
-        this.byte = new Laya.Byte();
-        this.byte.endian = Laya.Byte.LITTLE_ENDIAN;
+        this.createHttp();
+    }
+
+    private createHttp() {
+        let xhr = new Laya.HttpRequest();
+        xhr.http.timeout = 10000;
+        xhr.on(Laya.Event.PROGRESS, this, this.onHttpProgress);
+        xhr.once(Laya.Event.COMPLETE, this, this.onHttpComplete);
+        xhr.once(Laya.Event.ERROR, this, this.onHttpError);
+        xhr.send('http://localhost:8001/test.json', '', 'get', 'json');
+    }
+
+    private onHttpProgress(msg: any) {
+        console.log('连接HTTP进度');
+        console.log(msg);
+    }
+
+    private onHttpComplete(msg: any) {
+        console.log('连接HTTP成功');
+        console.log(msg);
+    }
+
+    private onHttpError(msg: any) {
+        console.log('连接HTTP错误');
+        console.log(msg);
+    }
+
+    private createWebsocket() {
+        this._byte = new Laya.Byte();
+        this._byte.endian = Laya.Byte.LITTLE_ENDIAN;
+
+        let so = new WebSocket('ws://127.0.0.1:9999');
+        so.binaryType = 'arraybuffer';
+        so.onopen = this.onOpen;
+        so.onmessage = this.onMessage;
+        so.onerror = this.onError;
+        so.onclose = this.onClose;
+        so.send('发送数据');
 
         // let so = new Laya.Socket('127.0.0.1', 9999);
         // so.endian = Laya.Byte.LITTLE_ENDIAN;
@@ -25,22 +61,7 @@ export class NetMgr extends BaseIns implements INetMgr {
         // so.on(Laya.Event.MESSAGE, this, this.onMessage);
         // so.on(Laya.Event.CLOSE, this, this.onClose);
         // so.on(Laya.Event.ERROR, this, this.onError);
-
-        // let so = new WebSocket('ws://127.0.0.1:9999');
-        // so.binaryType = 'arraybuffer';
-        // so.onopen = this.onOpen;
-        // so.onmessage = this.onMessage;
-        // so.onerror = this.onError;
-        // so.onclose = this.onClose;
-
         // so.send('发送数据');
-
-        let xhr = new Laya.HttpRequest();
-        xhr.http.timeout = 10000;
-        xhr.once(Laya.Event.COMPLETE, this, this.onOpen);
-        xhr.once(Laya.Event.ERROR, this, this.onError);
-        xhr.on(Laya.Event.PROGRESS, this, this.onMessage);
-        xhr.send('http://127.0.0.1:9999/root/index.html', 'id=120&name=李四&address=后海大道', 'post', 'json');
     }
 
     private onOpen() {
@@ -49,16 +70,17 @@ export class NetMgr extends BaseIns implements INetMgr {
 
     private onMessage(msg: any = null) {
         console.log('接收消息')
+        console.log(msg)
         // 这里我们假设收到的是二进制ArrayBuffer
-        this.byte.clear();
-        this.byte.writeArrayBuffer(msg); //把接收到的二进制数据读进byte数组便于解析。
-        this.byte.pos = 0; //设置偏移指针；
+        this._byte.clear();
+        this._byte.writeArrayBuffer(msg); //把接收到的二进制数据读进byte数组便于解析。
+        this._byte.pos = 0; //设置偏移指针；
         //下面开始读取数据，按照服务器传递过来的数据，按照顺序读取
-        let a: number = this.byte.getByte();
-        let b: number = this.byte.getInt16();
-        let c: number = this.byte.getFloat32();
-        let d: string = this.byte.getString();
-        let e: string = this.byte.getUTFString();
+        let a: number = this._byte.getByte();
+        let b: number = this._byte.getInt16();
+        let c: number = this._byte.getFloat32();
+        let d: string = this._byte.getString();
+        let e: string = this._byte.getUTFString();
     }
 
     private onError() {
